a70c1693889322a5f02cbee175d47e0623ab9177
nartsisss <daniilnartsissov@gmail.com>
2023-06-04 03:00:26 +0300

    build(gradle): dependency declaration by version catalog

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,6 @@
-val ktor_version: String by project
-val kotlin_version: String by project
-val logback_version: String by project
-
 plugins {
-    kotlin("jvm") version "1.8.21"
-    id("io.ktor.plugin") version "2.3.1"
+    alias(libs.plugins.kotlin)
+    alias(libs.plugins.ktor)
 }
 
 group = "xyz.krylex"
@@ -15,15 +11,10 @@ repositories {
 }
 
 dependencies {
-    implementation("io.ktor:ktor-client-core-jvm:$ktor_version")
-    implementation("io.ktor:ktor-client-cio-jvm:$ktor_version")
+    implementation(libs.jsoup)
+    implementation(libs.bundles.ktor)
+    implementation(libs.logback)
 
-    implementation("ch.qos.logback:logback-classic:$logback_version")
-
-    testImplementation("io.ktor:ktor-client-tests-jvm:$ktor_version")
-    testImplementation("org.jetbrains.kotlin:kotlin-test-junit:$kotlin_version")
-}
-
-application {
-    mainClass.set("xyz.krylex.ApplicationKt")
+    testImplementation(libs.bundles.ktor.tests)
+    testImplementation(libs.kotlin.junit)
 }
